5ab9146dec843cc3af6711dbae07157486e09a72
Nguyen Van Bin <bin2003mail@gmail.com>
2025-08-17 20:31:48 +0700

    test Vue

---
+++ vue-project/src/App.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  data() {
+    return {
+      name: 'John Doe',
+      like: 0,
+    };
+  },
+
+  methods: {
+    likeAndSub() {
+      this.like++;
+    }
+  },
+};
+</script>
+
+<template>
+  <div class="w-screen flex flex-col items-center justify-center h-screen bg-gray-100">
+    <h1 class="text-3xl font-bold text-blue-500">
+      Hi My name is {{ name }}
+    </h1>
+
+    <h2>
+      this is {{ like }} likes
+      <button v-on:click="likeAndSub()">Button</button>
+    </h2>
+  </div>
+</template>
+
+<style scoped></style>
