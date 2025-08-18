3c35f5516eee39b3a93cac47b3b2710139f53159
Nguyen Van Bin <bin2003mail@gmail.com>
2025-08-18 16:45:14 +0700

    component, slot

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,87 +1,19 @@
-<script>
-export default {
-  data() {
-    return {
-      name: 'John Doe',
-      like: 0,
-      active: false,
-      assignments: [
-        { name: 'Finish project', completed: false },
-        { name: 'Read chapter 4', completed: false },
-        { name: 'Turn in homework', completed: false }
-      ]
-    };
-  },
-
-  methods: {
-    likeAndSub() {
-      this.like++;
-      this.toggle();
-    },
-    toggle() {
-      this.active = !this.active;
-    }
-  },
-
-  computed: {
-    completedAssignment(){
-      return this.assignments.filter(assignment => assignment.completed);
-    },
-    inProgressAssignment(){
-      return this.assignments.filter(assignment => ! assignment.completed);
-    }
-  }
-};
+<script setup>
+import buttonCount from './compoents/buttonCount.vue';
+import buttonCountProps from './compoents/buttonCountProps.vue';
+import Slot from './compoents/Slot.vue';
 </script>
 
 <template>
-  <!-- <div class="w-screen flex flex-col items-center justify-center h-screen ">
-    <h2>
-      <button @click="toggle()" :class="active ? 'text-red-500' : 'text-blue-500'">
-        Button
-      </button>
-    </h2>
-  </div> -->
-
-  <div 
-    class="w-screen h-screen flex flex-row"
-    >
-
-    <div class="w-2/5 flex flex-col items-center justify-center h-auto"
-      v-show="inProgressAssignment.length">
-      <h2 class="mb-6 font-bold">assignments</h2>
-      <ul>
-        <li 
-          v-for="assignment in assignments.filter(a => ! a.completed)" 
-          :key="assignment.name">
-          <label>
-            {{ assignment.name }}
-            <input type="checkbox" v-model="assignment.completed" />
-          </label>
-        </li>
-      </ul>
-    </div>
-
-    <div 
-      class="w-2/5 flex flex-col items-center justify-center h-auto"
-      v-show="completedAssignment.length"
-      >
-      <h2 class="mb-6 font-bold">assignments</h2>
-      <ul>
-        <li v-for="assignment in completedAssignment" :key="assignment.name">
-          <label>
-            {{ assignment.name }}
-            <input type="checkbox" v-model="assignment.completed" />
-
-          </label>
-        </li>
-      </ul>
-    </div>
-
+  <div class="flex flex-col items-center justify-center h-screen">
+    <!-- <buttonCount />
+    <buttonCount />
+    <buttonCount />
+    <buttonCountProps name="John Doe" />
+    <buttonCountProps name="Jane Smith" />
+    <buttonCountProps name="Alice Johnson" /> -->
+    <Slot>
+      <p>THIS IS SLOT CONTENT</p>
+    </Slot>
   </div>
-
-
-
 </template>
-
-<style></style>
---
+++ vue-project/src/compoents/buttonCountProps.vue
@@ -0,0 +1,10 @@
+<script setup>
+defineProps(['name'])
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>Hi my name is {{ name }}</h1>
+</template>
