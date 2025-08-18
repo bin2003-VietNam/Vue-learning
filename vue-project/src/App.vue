bc6d978a7c809b8e3300d50e41f31c1eed6e5994
Nguyen Van Bin <bin2003mail@gmail.com>
2025-08-18 14:50:34 +0700

    v-for, v-model, v-show, computed properties in data, filter

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -5,6 +5,11 @@ export default {
       name: 'John Doe',
       like: 0,
       active: false,
+      assignments: [
+        { name: 'Finish project', completed: false },
+        { name: 'Read chapter 4', completed: false },
+        { name: 'Turn in homework', completed: false }
+      ]
     };
   },
 
@@ -13,21 +18,70 @@ export default {
       this.like++;
       this.toggle();
     },
-    toggle(){
-      this.active = ! this.active;
+    toggle() {
+      this.active = !this.active;
     }
   },
+
+  computed: {
+    completedAssignment(){
+      return this.assignments.filter(assignment => assignment.completed);
+    },
+    inProgressAssignment(){
+      return this.assignments.filter(assignment => ! assignment.completed);
+    }
+  }
 };
 </script>
 
 <template>
-  <div class="w-screen flex flex-col items-center justify-center h-screen ">
+  <!-- <div class="w-screen flex flex-col items-center justify-center h-screen ">
     <h2>
       <button @click="toggle()" :class="active ? 'text-red-500' : 'text-blue-500'">
         Button
       </button>
     </h2>
+  </div> -->
+
+  <div 
+    class="w-screen h-screen flex flex-row"
+    >
+
+    <div class="w-2/5 flex flex-col items-center justify-center h-auto"
+      v-show="inProgressAssignment.length">
+      <h2 class="mb-6 font-bold">assignments</h2>
+      <ul>
+        <li 
+          v-for="assignment in assignments.filter(a => ! a.completed)" 
+          :key="assignment.name">
+          <label>
+            {{ assignment.name }}
+            <input type="checkbox" v-model="assignment.completed" />
+          </label>
+        </li>
+      </ul>
+    </div>
+
+    <div 
+      class="w-2/5 flex flex-col items-center justify-center h-auto"
+      v-show="completedAssignment.length"
+      >
+      <h2 class="mb-6 font-bold">assignments</h2>
+      <ul>
+        <li v-for="assignment in completedAssignment" :key="assignment.name">
+          <label>
+            {{ assignment.name }}
+            <input type="checkbox" v-model="assignment.completed" />
+
+          </label>
+        </li>
+      </ul>
+    </div>
+
   </div>
+
+
+
 </template>
 
 <style></style>
