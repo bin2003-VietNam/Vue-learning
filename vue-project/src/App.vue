9cd5cd6493cfdc00b2e7ea5a6d0d5a52b8aaa228
Nguyen Van Bin <bin2003mail@gmail.com>
2025-08-18 13:55:49 +0700

    v-on and v-bind

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -4,28 +4,30 @@ export default {
     return {
       name: 'John Doe',
       like: 0,
+      active: false,
     };
   },
 
   methods: {
     likeAndSub() {
       this.like++;
+      this.toggle();
+    },
+    toggle(){
+      this.active = ! this.active;
     }
   },
 };
 </script>
 
 <template>
-  <div class="w-screen flex flex-col items-center justify-center h-screen bg-gray-100">
-    <h1 class="text-3xl font-bold text-blue-500">
-      Hi My name is {{ name }}
-    </h1>
-
+  <div class="w-screen flex flex-col items-center justify-center h-screen ">
     <h2>
-      this is {{ like }} likes
-      <button v-on:click="likeAndSub()">Button</button>
+      <button @click="toggle()" :class="active ? 'text-red-500' : 'text-blue-500'">
+        Button
+      </button>
     </h2>
   </div>
 </template>
 
-<style scoped></style>
+<style></style>
