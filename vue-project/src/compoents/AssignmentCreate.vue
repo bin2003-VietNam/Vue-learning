3e1d7dfc4a681cc663a72865206170cde597100f
Nguyen Van Bin <bin2003mail@gmail.com>
2025-08-19 14:56:39 +0700

    use $emit to pass event/data from child component to parent component

---
+++ vue-project/src/compoents/AssignmentCreate.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    props:{
+        assignments: Array
+    },
+    data() {
+        return {
+            newAssignment: ''
+        };
+    },
+    methods: {
+        add() {
+            this.$emit('add', this.newAssignment);
+            this.newAssignment = ""
+        }
+    }
+
+};
+</script>
+<template>
+    <form @submit.prevent="add">
+        <div class="border border-gray-500">
+            <input v-model="newAssignment" type="text" placeholder="New Assignment" class="text-black p-2" />
+            <button type="submit" class="bg-white text-black p-2 bl-2">Submit</button>
+        </div>
+    </form>
+</template>
--- vue-project/src/compoents/Assignments.vue
+++ vue-project/src/compoents/Assignments.vue
@@ -1,50 +1,52 @@
 <script>
 import AssignmentList from './AssignmentList.vue';
+import AssignmentCreate from './AssignmentCreate.vue';
 
 export default {
-  data() {
-    return {
-      name: 'John Doe',
-      like: 0,
-      active: false,
-      assignments: [
-        { name: 'Finish project', completed: false },
-        { name: 'Read chapter 4', completed: false },
-        { name: 'Turn in homework', completed: false }
-      ],
-      newAssignment: ''
-    };
-  },
-
-  methods: {
-    likeAndSub() {
-      this.like++;
-      this.toggle();
-    },
-    toggle() {
-      this.active = !this.active;
-    },
-    add(){
-        this.assignments.push(({
-            name: this.newAssignment,
-            completed: false
-        }));
-        this.newAssignment= '';
-    }
-  },
-
-  computed: {
-    filters(){
+    data() {
         return {
-            completed: this.assignments.filter(assignment => assignment.completed),
-            inProgress: this.assignments.filter(assignment => ! assignment.completed)
-        }
-    }
-  },
+            name: 'John Doe',
+            like: 0,
+            active: false,
+            assignments: [
+                { name: 'Finish project', completed: false },
+                { name: 'Read chapter 4', completed: false },
+                { name: 'Turn in homework', completed: false }
+            ],
+        };
+    },
 
-  components: {
-    AssignmentList
-  }
+    methods: {
+        likeAndSub() {
+            this.like++;
+            this.toggle();
+        },
+        toggle() {
+            this.active = !this.active;
+        },
+        add(name) {
+            this.assignments.push(({
+                name: name,
+                completed: false
+            }));
+            this.newAssignment = ""
+        }
+
+    },
+
+    computed: {
+        filters() {
+            return {
+                completed: this.assignments.filter(assignment => assignment.completed),
+                inProgress: this.assignments.filter(assignment => !assignment.completed)
+            }
+        }
+    },
+
+    components: {
+        AssignmentList,
+        AssignmentCreate
+    }
 };
 </script>
 
@@ -52,11 +54,6 @@ export default {
     <div class="flex flex-col items-center justify-center w-full h-full">
         <AssignmentList :assignments="filters.inProgress" title="inProgress Assignment" />
         <AssignmentList :assignments="filters.completed" title="Completed Assignment" />
-        <form @submit.prevent="add">
-            <div class="border border-gray-500">
-                <input v-model="newAssignment" type="text" placeholder="New Assignment" class="text-black p-2"/>
-                <button type="submit" class="bg-white text-black p-2 bl-2">Submit</button>
-            </div>
-        </form>
+        <AssignmentCreate @add="add" />
     </div>
 </template>
