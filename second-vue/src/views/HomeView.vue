8311a3e263a5748ac3bbb090bcb14f017bef7318
Nguyen Van Bin <bin2003mail@gmail.com>
2025-08-22 11:51:11 +0700

    API composition

--- second-vue/src/views/HomeView.vue
+++ second-vue/src/views/HomeView.vue
@@ -1,9 +1,27 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import TheWelcome from '@/components/TheWelcome.vue'
+import { onMounted } from 'vue';
+import { ref } from 'vue';
+
+let message = ref('hello world');
+
+let doSomgthing = (a, b) => {
+  return a+b
+}
+
+let clickMe = () => {
+  alert('clicked')
+}
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <the-welcome />
+    <p>{{ message }} + {{ doSomgthing(1,2) }}</p>
+    <P>
+      <input type="text" v-model="message" />
+      <button @click="clickMe">Button</button>
+    </P>
   </main>
 </template>
